8fad1307ccb13830c547fa7c29d87d9c6b2c6a78
Jona Motta <jona.motta@polytechnique.edu>
2022-08-20 16:55:10 +0200

    update gitignore

--- L1TauMinatorEmulator/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/L1TauMinator/L1TauMinatorEmulator/',1)[0])+'/cfipython/slc7_amd64_gcc10/L1TauMinator/L1TauMinatorEmulator')
