78ddd36b7da3b2bfcb6b924119c0e0de32fe9cce
Jona Motta <jona.motta@polytechnique.edu>
2022-08-22 18:36:33 +0200

    update the merger not to depend on the tagfile

--- L1TauMinatorSoftware/InputMaker/Merger.py
+++ L1TauMinatorSoftware/InputMaker/Merger.py
@@ -1,5 +1,7 @@
 from optparse import OptionParser
 import numpy as np
+import random
+import glob
 import os
 
 #######################################################################
@@ -19,6 +21,8 @@ if __name__ == "__main__" :
     parser.add_option('--doQCD',        dest='doQCD',        action='store_true', default=False)
     parser.add_option('--doVBFH',       dest='doVBFH',       action='store_true', default=False)
     parser.add_option('--doMinBias',    dest='doMinBias',    action='store_true', default=False)
+    parser.add_option('--doZp500',      dest='doZp500',      action='store_true', default=False)
+    parser.add_option('--doZp1500',     dest='doZp1500',     action='store_true', default=False)
     parser.add_option('--doTestRun',    dest='doTestRun',    action='store_true', default=False)
     parser.add_option('--doTens4Calib', dest='doTens4Calib', action='store_true', default=None)
     parser.add_option('--doTens4Ident', dest='doTens4Ident', action='store_true', default=None)
@@ -35,7 +39,7 @@ if __name__ == "__main__" :
         print('** EXITING')
         exit()
 
-    if not options.doHH and not options.doQCD and not options.doVBFH and not options.doMinBias and not options.doTestRun:
+    if not options.doHH and not options.doQCD and not options.doVBFH and not options.doMinBias and not options.doZp500 and not options.doZp1500 and not options.doTestRun:
         print('** ERROR : no matching dataset specified. What do you want to do (doHH, doQCD, doVBFH, doMinBias, doTestRun)?')
         print('** EXITING')
         exit()
@@ -46,25 +50,85 @@ if __name__ == "__main__" :
     indirs = []
     taglists = []
 
+    if options.doTens4Calib: splitter = 'X_Calibrator'
+    if options.doTens4Ident: splitter = 'X_Identifier'
+
     if options.doHH:
-        indirs.append(indir+'/GluGluToHHTo2B2Tau_node_SM_14TeV-madgraph-pythia8_tuneCP5__Phase2HLTTDRWinter20DIGI-PU200_110X_mcRun4_realistic_v3-v2__GEN-SIM-DIGI-RAW__batches/TensorizedInputs_'+options.caloClNxM+options.inTag)
-        taglists.append(open(indir+'/GluGluToHHTo2B2Tau_node_SM_14TeV-madgraph-pythia8_tuneCP5__Phase2HLTTDRWinter20DIGI-PU200_110X_mcRun4_realistic_v3-v2__GEN-SIM-DIGI-RAW__batches/tagsFile.txt'))
+        tmp = indir+'/GluGluToHHTo2B2Tau_node_SM_14TeV-madgraph-pythia8_tuneCP5__Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_111X_mcRun4_realistic_T15_v1-v1__GEN-SIM-DIGI-RAW-MINIAOD__batches/TensorizedInputs_'+options.caloClNxM+options.inTag
+        indirs.append(tmp)
+
+        taglist = []
+        files = glob.glob(tmp+'/'+splitter+'*.npz')
+        for file in files:
+            tag = '_'+file.split(splitter)[1].split('_')[1].split('.')[0]
+            taglist.append(tag)
+        taglists.append(taglist)
 
     if options.doVBFH:
-        indirs.append(indir+'/VBFHToTauTau_M125_14TeV_powheg_pythia8_correctedGridpack_tuneCP5__Phase2HLTTDRWinter20DIGI-PU200_110X_mcRun4_realistic_v3-v3__GEN-SIM-DIGI-RAW__batches/TensorizedInputs_'+options.caloClNxM+options.inTag)
-        taglists.append(open(indir+'/VBFHToTauTau_M125_14TeV_powheg_pythia8_correctedGridpack_tuneCP5__Phase2HLTTDRWinter20DIGI-PU200_110X_mcRun4_realistic_v3-v3__GEN-SIM-DIGI-RAW__batches/tagsFile.txt'))
+        tmp = indir+'/VBFHToTauTau_M125_14TeV_powheg_pythia8_correctedGridpack_tuneCP5__Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_111X_mcRun4_realistic_T15_v1-v1__FEVT__batches/TensorizedInputs_'+options.caloClNxM+options.inTag
+        indirs.append(tmp)
+        
+        taglist = []
+        files = glob.glob(tmp+'/'+splitter+'*.npz')
+        for file in files:
+            tag = '_'+file.split(splitter)[1].split('_')[1].split('.')[0]
+            taglist.append(tag)
+        taglists.append(taglist)
 
     if options.doQCD:
-        indirs.append(indir+'/QCD_Pt-15to3000_TuneCP5_Flat_14TeV-pythia8__Phase2HLTTDRWinter20DIGI-PU200_castor_110X_mcRun4_realistic_v3-v2__GEN-SIM-DIGI-RAW__batches/TensorizedInputs_'+options.caloClNxM+options.inTag)
-        taglists.append(open(indir+'/QCD_Pt-15to3000_TuneCP5_Flat_14TeV-pythia8__Phase2HLTTDRWinter20DIGI-PU200_castor_110X_mcRun4_realistic_v3-v2__GEN-SIM-DIGI-RAW__batches/tagsFile.txt'))
+        tmp = indir+'/QCD_Pt-15to3000_TuneCP5_Flat_14TeV-pythia8__Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_castor_111X_mcRun4_realistic_T15_v1-v1__FEVT__batches/TensorizedInputs_'+options.caloClNxM+options.inTag
+        indirs.append(tmp)
+
+        taglist = []
+        files = glob.glob(tmp+'/'+splitter+'*.npz')
+        for file in files:
+            tag = '_'+file.split(splitter)[1].split('_')[1].split('.')[0]
+            taglist.append(tag)
+        taglists.append(taglist)
 
     if options.doMinBias:
-        indirs.append(indir+'/MinBias_TuneCP5_14TeV-pythia8__Phase2HLTTDRWinter20DIGI-PU200_110X_mcRun4_realistic_v3-v3__GEN-SIM-DIGI-RAW__batches/TensorizedInputs_'+options.caloClNxM+options.inTag)
-        taglists.append(open(indir+'/MinBias_TuneCP5_14TeV-pythia8__Phase2HLTTDRWinter20DIGI-PU200_110X_mcRun4_realistic_v3-v3__GEN-SIM-DIGI-RAW__batches/tagsFile.txt'))
+        tmp = indir+'/MinBias_TuneCP5_14TeV-pythia8__Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_withNewMB_111X_mcRun4_realistic_T15_v1_ext1-v2__FEVT__batches/TensorizedInputs_'+options.caloClNxM+options.inTag
+        indirs.append(tmp)
+
+        taglist = []
+        files = glob.glob(tmp+'/'+splitter+'*.npz')
+        for file in files:
+            tag = '_'+file.split(splitter)[1].split('_')[1].split('.')[0]
+            taglist.append(tag)
+        taglists.append(taglist)
+
+    if options.doZp500:
+        tmp = indir+'/ZprimeToTauTau_M-500_TuneCP5_14TeV-pythia8-tauola__Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_111X_mcRun4_realistic_T15_v1-v1__GEN-SIM-DIGI-RAW-MINIAOD__batches/TensorizedInputs_'+options.caloClNxM+options.inTag
+        indirs.append(tmp)
+
+        taglist = []
+        files = glob.glob(tmp+'/'+splitter+'*.npz')
+        for file in files:
+            tag = '_'+file.split(splitter)[1].split('_')[1].split('.')[0]
+            taglist.append(tag)
+        taglists.append(taglist)
+
+    if options.doZp1500:
+        tmp = indir+'/ZprimeToTauTau_M-1500_TuneCP5_14TeV-pythia8-tauola__Phase2HLTTDRSummer20ReRECOMiniAOD-PU200_111X_mcRun4_realistic_T15_v1-v1__GEN-SIM-DIGI-RAW-MINIAOD__batches/TensorizedInputs_'+options.caloClNxM+options.inTag
+        indirs.append(tmp)
+
+        taglist = []
+        files = glob.glob(tmp+'/'+splitter+'*.npz')
+        for file in files:
+            tag = '_'+file.split(splitter)[1].split('_')[1].split('.')[0]
+            taglist.append(tag)
+        taglists.append(taglist)
 
     if options.doTestRun:
-        indirs.append(indir+'/test__batches/TensorizedInputs_'+options.caloClNxM+options.inTag)
-        taglists.append(open(indir+'/test__batches/tagsFile.txt'))
+        tmp = indir+'/test__batches/TensorizedInputs_'+options.caloClNxM+options.inTag
+        indirs.append(tmp)
+
+        taglist = []
+        files = glob.glob(tmp+'/'+splitter+'*.npz')
+        for file in files:
+            tag = '_'+file.split(splitter)[1].split('_')[1].split('.')[0]
+            taglist.append(tag)
+        taglists.append(taglist)
 
 
     outdir = indir + '/TauCNN'
@@ -80,7 +144,6 @@ if __name__ == "__main__" :
         'targetsCalibrator' : '/Y_Calibrator'+options.caloClNxM,
         'targetsIdentifier' : '/Y_Identifier'+options.caloClNxM
     }
-
 
     XsToConcatenate = []
     YsToConcatenate = []
@@ -109,6 +172,8 @@ if __name__ == "__main__" :
                 # DEBUG
                 print('** INFO: towers'+tag+' not found --> skipping')
                 continue
+
+            if idx==50: break
 
     X = np.concatenate(XsToConcatenate)
     Y = np.concatenate(YsToConcatenate)
--- L1TauMinatorSoftware/InputMaker/MergerSubmitter.sh
+++ L1TauMinatorSoftware/InputMaker/MergerSubmitter.sh
@@ -1,9 +1,14 @@
-python Merger.py --v 0 --date 2022_08_17 --caloClNxM 9x9 --doTens4Calib --doTestRun
-python Merger.py --v 0 --date 2022_08_17 --caloClNxM 7x7 --doTens4Calib --doTestRun
-python Merger.py --v 0 --date 2022_08_17 --caloClNxM 5x5 --doTens4Calib --doTestRun
-python Merger.py --v 0 --date 2022_08_17 --caloClNxM 5x9 --doTens4Calib --doTestRun
+# python Merger.py --v 0 --date 2022_08_17 --caloClNxM 9x9 --doTens4Calib --doTestRun
+# python Merger.py --v 0 --date 2022_08_17 --caloClNxM 7x7 --doTens4Calib --doTestRun
+# python Merger.py --v 0 --date 2022_08_17 --caloClNxM 5x5 --doTens4Calib --doTestRun
+# python Merger.py --v 0 --date 2022_08_17 --caloClNxM 5x9 --doTens4Calib --doTestRun
 
-python Merger.py --v 0 --date 2022_08_17 --caloClNxM 9x9 --doTens4Ident --doTestRun
-python Merger.py --v 0 --date 2022_08_17 --caloClNxM 7x7 --doTens4Ident --doTestRun
-python Merger.py --v 0 --date 2022_08_17 --caloClNxM 5x5 --doTens4Ident --doTestRun
-python Merger.py --v 0 --date 2022_08_17 --caloClNxM 5x9 --doTens4Ident --doTestRun
+# python Merger.py --v 0 --date 2022_08_17 --caloClNxM 9x9 --doTens4Ident --doTestRun
+# python Merger.py --v 0 --date 2022_08_17 --caloClNxM 7x7 --doTens4Ident --doTestRun
+# python Merger.py --v 0 --date 2022_08_17 --caloClNxM 5x5 --doTens4Ident --doTestRun
+# python Merger.py --v 0 --date 2022_08_17 --caloClNxM 5x9 --doTens4Ident --doTestRun
+
+
+python Merger.py --v 1 --date 2022_08_20 --caloClNxM 9x9 --doTens4Calib --doHH --doVBFH --doZp500 --doZp1500
+
+python Merger.py --v 1 --date 2022_08_20 --caloClNxM 9x9 --doTens4Ident --doHH --doVBFH --doQCD --doZp500 --doZp1500
